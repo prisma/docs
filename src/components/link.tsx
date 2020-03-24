eec6838112d89178455d86344da1c08add793981
Nilu <nilubava@gmail.com>
2020-03-24 16:05:24 +0100

    Active lnk using gatsby lnk

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link as GatsbyLink } from 'gatsby';
 
-const Link = ({ to, ...props }: any) =>
+const Link = ({ to, activeClassName, ...props }: any) =>
   !to ? (
     <a href={to} {...props}>
       {props.children}
     </a>
   ) : (
-    <GatsbyLink to={to} {...props} />
+    <GatsbyLink activeClassName={activeClassName} to={to} {...props} />
   );
 
 export default Link;
