3dbe397df2ace115babd9a325fe037ee27539027
Nilu <nilubava@gmail.com>
2020-03-30 20:55:50 +0200

    Removed index.tsx + 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,13 +6,19 @@ const NotFoundWrapper = styled.div`
   font-family: 'Open Sans';
   margin-top: 200px;
   padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    font-weight: bold;
+  }
 `;
 
 const NotFoundPage = () => (
   <Layout>
     <NotFoundWrapper>
-      <h1>NOT FOUND :(</h1>
-      <i>You just hit a route that doesn&#39;t exist... the sadness.</i>
+      <h1>404 | NOT FOUND</h1>
+      You just hit a route that doesn&#39;t exist!
     </NotFoundWrapper>
   </Layout>
 );
