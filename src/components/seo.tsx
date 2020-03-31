787f7387ad4209350a9fcb6d7a16aff005073c15
Matthew Mueller <mattmuelle@gmail.com>
2020-03-31 15:28:02 +0200

    fix for mobile (#86)

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -18,8 +18,8 @@ const SEO = ({ title, description, keywords, slug }: SEOProps) => {
   canonicalUrl = slug ? canonicalUrl + slug : canonicalUrl;
   return (
     <Helmet>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      {/* <meta charSet="utf-8" /> */}
+      {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
       {title && <title>{title}</title>}
       {description && <meta name="title" content={description} />}
       {description && <meta name="description" content={description} />}
