89ef8f38a2dbe1065603a93198a9be062848d761
Nilu <nilubava@gmail.com>
2020-04-02 13:39:38 +0200

    Link for null values

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Link as GatsbyLink } from 'gatsby';
 import isAbsoluteUrl from 'is-absolute-url';
 
-const Link = ({ to, ...props }: any) =>{
-  return to && isAbsoluteUrl(to) ? (
+interface LinkProps {
+  to: string | null;
+  activeClassName?: string;
+}
+
+const Link = ({ to, activeClassName, ...props }: LinkProps & React.ReactNode) =>
+  !to || isAbsoluteUrl(to) ? (
     <a href={to} {...props}>
       {props.children}
     </a>
   ) : (
-    <GatsbyLink to={to} {...props} />
+    <GatsbyLink to={to} activeClassName={activeClassName} {...props} />
   );
-}
+
 export default Link;
