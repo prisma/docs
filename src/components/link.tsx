003c3e2b9cd7ee3c7ff5ccdbcafdfdb73b305943
Nilu <nilubava@gmail.com>
2020-04-02 11:52:08 +0200

    Absolute/relative links

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,13 +1,29 @@
+// import React from 'react';
+// import { Link as GatsbyLink } from 'gatsby';
+
+// const Link = ({ to, activeClassName, ...props }: any) =>
+//   !to ? (
+//     <a href={to} {...props}>
+//       {props.children}
+//     </a>
+//   ) : (
+//     <GatsbyLink activeClassName={activeClassName} to={to} {...props} />
+//   );
+
+// export default Link;
+
 import React from 'react';
 import { Link as GatsbyLink } from 'gatsby';
+import isAbsoluteUrl from 'is-absolute-url';
 
-const Link = ({ to, activeClassName, ...props }: any) =>
-  !to ? (
+const Link = ({ to, ...props }: any) => {
+  console.log(to);
+  return to && isAbsoluteUrl(to) ? (
     <a href={to} {...props}>
       {props.children}
     </a>
   ) : (
-    <GatsbyLink activeClassName={activeClassName} to={to} {...props} />
+    <GatsbyLink to={to} {...props} />
   );
-
+};
 export default Link;
