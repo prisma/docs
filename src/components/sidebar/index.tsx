119da9f2328c73c195b1bd7bba4dcd8222592aac
Nilu <nilubava@gmail.com>
2020-04-06 16:28:38 +0200

    Sticky nav

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,28 +1,47 @@
 import React from 'react';
 import Tree from './tree';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import styledTS from 'styled-components-ts';
+import { StickyContainer, Sticky } from 'react-sticky';
 import { useAllArticlesQuery } from '../../hooks/useAllArticlesQuery';
 import { AllArticles } from '../../interfaces/AllArticles.interface';
 
-const Sidebar = styled.aside`
+const SidebarContainer = styled.aside`
+  width: 235px;
+`;
+
+const Sidebar = styledTS<{ isSticky: boolean }>(styled.div)`
   margin: 0;
-  max-width: 215px;
-  width: 20%;
+  overflow: auto;
+  ${({ isSticky }: any) =>
+    isSticky &&
+    css`
+      max-height: 100vh;
+    `};
 `;
 
 const List = styled.ul`
   list-style: none;
-  padding: 0;
+  padding: 0 0 0 20px;
+  margin: 0;
 `;
 
 const SidebarLayout = () => {
   const { allMdx }: AllArticles = useAllArticlesQuery();
   return (
-    <Sidebar>
-      <List>
-        <Tree edges={allMdx.edges} />
-      </List>
-    </Sidebar>
+    <StickyContainer>
+      <SidebarContainer>
+        <Sticky topOffset={0}>
+          {({ style, isSticky }: any) => (
+            <Sidebar style={style} isSticky={isSticky}>
+              <List>
+                <Tree edges={allMdx.edges} />
+              </List>
+            </Sidebar>
+          )}
+        </Sticky>
+      </SidebarContainer>
+    </StickyContainer>
   );
 };
 
