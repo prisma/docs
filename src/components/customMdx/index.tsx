4e1c981db7710c086991747f5e35ee34bea9d9cc
Nilu <nilubava@gmail.com>
2020-04-09 15:30:17 +0200

    Added autolinker

--- src/components/customMdx/index.tsx
+++ src/components/customMdx/index.tsx
@@ -9,11 +9,11 @@ import makeHeading from './headings';
 
 export default {
   h1: () => <h1 style={{ display: 'none' }} />,
-  h2: (props: any) => makeHeading('h2')({ ...props }),
-  h3: (props: any) => makeHeading('h3')({ ...props }),
-  h4: (props: any) => makeHeading('h4')({ ...props }),
-  h5: (props: any) => makeHeading('h5')({ ...props }),
-  h6: (props: any) => makeHeading('h6')({ ...props }),
+  // h2: (props: any) => makeHeading('h2')({ ...props }),
+  // h3: (props: any) => makeHeading('h3')({ ...props }),
+  // h4: (props: any) => makeHeading('h4')({ ...props }),
+  // h5: (props: any) => makeHeading('h5')({ ...props }),
+  // h6: (props: any) => makeHeading('h6')({ ...props }),
   p: (props: any) => <p className="paragraph" {...props} />,
   ul: (props: any) => <ul className="list" {...props} />,
   CodeBlock,
