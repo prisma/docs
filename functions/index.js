ade13efa968f9d6db1fba1547f58394bccbdb720
timsuchanek <tim.suchanek@gmail.com>
2020-04-14 15:35:34 +0200

    add function

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+const { PrismaClient, prismaVersion } = require('@prisma/client');
+
+const client = new PrismaClient();
+
+exports.handler = async function(event, context, callback) {
+  const data = await client.feedback.findMany();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  };
+};
