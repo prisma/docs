f9964ed2f6d3fea10d299d53518fb00c672190ce
timsuchanek <tim.suchanek@gmail.com>
2020-04-14 16:26:06 +0200

    update function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,26 @@
 const { PrismaClient, prismaVersion } = require('@prisma/client');
+const yup = require('yup');
 
 const client = new PrismaClient();
 
+const schema = yup.object().shape({
+  pageUrl: yup.string().require(),
+  sentiment: yup.string().require(),
+});
+
 exports.handler = async function(event, context, callback) {
-  const data = await client.feedback.findMany();
+  const body = JSON.parse(event.body);
+
+  await schema.isValid(body);
+
+  const data = await client.feedback.create({
+    data: {
+      ip: event.headers['x-forwarded-for'],
+      pageUrl: body.pageUrl,
+      userAgent: event.headers['user-agent'],
+      sentiment: body.sentiment,
+    },
+  });
 
   return {
     statusCode: 200,
