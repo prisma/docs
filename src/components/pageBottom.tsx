4c8066362be2d2b1687662973af6401a377fce65
Nilu <nilubava@gmail.com>
2020-04-14 22:03:05 +0200

    Added prefix from config

--- src/components/pageBottom.tsx
+++ src/components/pageBottom.tsx
@@ -53,11 +53,7 @@ const PageBottom = ({ editDocsPath, pageUrl }: any) => {
       body,
     };
 
-<<<<<<< HEAD
     fetch(`${config.gatsby.pathPrefix}/.netlify/functions/index`, requestOptions)
-=======
-    fetch('/docs/.netlify/functions/index', requestOptions)
->>>>>>> b78791831559f03549cb7cb068ea5042eb00662e
       .then((response: any) => {
         setIsSubmitting(false);
         return response.text();
