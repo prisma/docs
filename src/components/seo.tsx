e3266b1ed5e6ad213b91e0ad24762c8a1f208339
Nilu <nilubava@gmail.com>
2020-04-15 15:09:23 +0200

    Added path

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -16,7 +16,7 @@ const SEO = ({ title, description, keywords, slug }: SEOProps) => {
   let canonicalUrl = config.gatsby.siteUrl;
   canonicalUrl = config.gatsby.pathPrefix ? canonicalUrl + config.gatsby.pathPrefix : canonicalUrl;
   canonicalUrl = slug ? canonicalUrl + urlGenerator(slug) : canonicalUrl;
-  const siteUrl = config.gatsby.siteUrl + config.gatsby.pathPrefix;
+  const siteUrl = config.gatsby.siteUrl;
   return (
     <Helmet>
       {/* <meta charSet="utf-8" /> */}
