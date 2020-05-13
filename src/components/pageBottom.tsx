e344e2f4de3abe1af4f9de30721e7e9400461672
Nilu <nilubava@gmail.com>
2020-05-13 11:30:19 +0200

    old url

--- src/components/pageBottom.tsx
+++ src/components/pageBottom.tsx
@@ -122,7 +122,7 @@ const PageBottom = ({ editDocsPath }: any) => {
       method: 'POST',
       body,
     }
-    await fetch(`.netlify/functions/index`, requestOptions)
+    await fetch(`https://prisma2.netlify.com/.netlify/functions/index`, requestOptions)
   }
 
   const handleSentiment = (e: any) => {
