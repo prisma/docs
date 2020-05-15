5de1c74c11d9844eea6eb626a545ca0b48a7337c
Daniel Norman <daniel@2color.me>
2020-05-15 20:25:16 +0200

    Improve analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,9 @@
-// require('prismjs/themes/prism.css');
+const { init, trackPage } = require('./src/utils/stats')
+
+exports.onClientEntry = () => {
+  init()
+}
+
+exports.onRouteUpdate = ({ location }) => {
+  trackPage(location.pathname)
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -48,13 +48,6 @@ module.exports = {
     docsLocation: config.siteMetadata.docsLocation,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-74131346-14',
-        anonymize: true,
-      },
-    },
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
