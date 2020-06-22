16b12b01f719b40f5d1e2129439586a3b4c10052
Nilufar Bava <nilubava@gmail.com>
2020-06-22 17:19:40 +0200

    fix: windows link checker (#493)
    * fix: windows link checker
    
    * fixed url
    
    * fix for Mac
    
    * fix for Mac

--- plugins/gatsby-remark-check-links-numberless/index.js
+++ plugins/gatsby-remark-check-links-numberless/index.js
@@ -32,7 +32,7 @@ module.exports = async function plugin(
   { exceptions = [], ignore = [], verbose = true } = {}
 ) {
   const withPathPrefix = createPathPrefixer(pathPrefix)
-  const pathSep = path.sep || '/'
+  const pathSep = '/'
   if (!markdownNode.fields) {
     // let the file pass if it has no fields
     return markdownAST
@@ -62,7 +62,10 @@ module.exports = async function plugin(
   const parent = await getNode(markdownNode.parent)
   const setAt = Date.now()
   cache.set(getCacheKey(parent), {
-    path: withPathPrefix(markdownNode.fields.slug.replace(/\d+-/g, '').concat(pathSep)),
+    path:
+      pathPrefix === ''
+        ? markdownNode.fields.slug.replace(/\d+-/g, '')
+        : withPathPrefix(markdownNode.fields.slug.replace(/\d+-/g, '').concat(pathSep)),
     links,
     headings,
     setAt,
@@ -98,8 +101,9 @@ module.exports = async function plugin(
   const prefixedIgnore = ignore.map(withPathPrefix)
   const prefixedExceptions = exceptions.map(withPathPrefix)
   const pathKeys = Object.keys(linksMap)
-  const pathKeysWithoutIndex = pathKeys.map(p => p.replace(`index${pathSep}`, '').replace(/.$/,''))
-
+  const pathKeysWithoutIndex = pathKeys.map(p =>
+    p.replace(`${pathSep}index`, '').replace(/\/$/, '')
+  )
   for (const pathL in linksMap) {
     if (prefixedIgnore.includes(pathL)) {
       // don't count broken links for ignored pages
@@ -109,7 +113,6 @@ module.exports = async function plugin(
     const linksForPath = linksMap[pathL]
     if (linksForPath.length) {
       const brokenLinks = linksForPath.filter(link => {
-
         // return true for broken links, false = pass
         const { key, hasHash, hashIndex } = getHeadingsMapKey(link.tranformedUrl, pathL)
         if (prefixedExceptions.includes(key)) {
@@ -128,8 +131,7 @@ module.exports = async function plugin(
 
           return false
         }
-    
-        return !pathKeysWithoutIndex.includes(urlToCheck) 
+        return !pathKeysWithoutIndex.includes(urlToCheck)
       })
 
       const brokenLinkCount = brokenLinks.length
