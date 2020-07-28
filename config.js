1fce0f96dbc877a566a6b28b96651b6c2ec348a8
mhwelander <welander@prisma.io>
2020-07-28 09:57:17 +0200

    Title suffix

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
     pathPrefix: '/docs',
     siteUrl: 'https://www.prisma.io',
     titlePrefix: '',
-    titleSuffix: '',
+    titleSuffix: ' | Prisma Documentation',
   },
   redirects: [
     {
