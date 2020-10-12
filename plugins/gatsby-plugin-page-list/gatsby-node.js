3cd6966550ad9d66cb405d75de97cdbf7d911094
Daniel Norman <daniel@2color.me>
2020-10-12 17:05:24 +0200

    Remove redirections from the page listing

--- plugins/gatsby-plugin-page-list/gatsby-node.js
+++ plugins/gatsby-plugin-page-list/gatsby-node.js
@@ -4,6 +4,15 @@ const url = require('url')
 
 const publicPath = `./public`
 
+// Excluded paths from page listing as they're redirected to the page with the language and db selected
+// https://github.com/prisma/homepage-v8/blob/b972182ebee2c824e6b2981088907b215d9bfee0/public/_redirects#L75-L78
+const excludedPaths = [
+  '/getting-started/quickstart',
+  '/getting-started/setup-prisma/add-to-existing-project',
+  '/getting-started/setup-prisma/start-from-scratch-sql ',
+  '/getting-started/setup-prisma/start-from-scratch-prisma-migrate',
+]
+
 exports.onPostBuild = async ({ graphql, pathPrefix, basePath = pathPrefix }, pluginOptions) => {
   const outputFile = path.join(publicPath, '/pages.json')
 
@@ -15,23 +24,12 @@ exports.onPostBuild = async ({ graphql, pathPrefix, basePath = pathPrefix }, plu
           pathPrefix
         }
       }
-      allMdx {
-        edges {
-          node {
-            frontmatter {
-              title
-            }
-            fields {
-              id
-            }
-          }
-        }
-      }
       allSitePage {
         edges {
           node {
             path
             context {
+              seoTitle
               id
             }
           }
@@ -40,23 +38,24 @@ exports.onPostBuild = async ({ graphql, pathPrefix, basePath = pathPrefix }, plu
   }`
   const { data } = await graphql(query)
 
-  // Create an object mapping the id -> path
-  const pathById = {}
-  data.allSitePage.edges.forEach(edge => {
-    // Some pages have no ID, e.g. 404 pages.
-    if (!edge.node.context) return
-    pathById[edge.node.context.id] = edge.node.path
-  })
-
   // Construct the pages json by iterating over the mdx files.
-  const pages = data.allMdx.edges.map((edge, i) => {
-    return {
-      title: edge.node.frontmatter.title,
-      url: url.resolve(
-        data.site.siteMetadata.siteUrl,
-        path.join(data.site.siteMetadata.pathPrefix, pathById[edge.node.fields.id])
-      ),
-    }
-  })
+  const pages = data.allSitePage.edges
+    .map((edge, i) => {
+      // Skip the 404 pages and pages without seoTitle
+      if (!edge.node.context || !edge.node.context.seoTitle) return null
+      console.log(edge.node.path)
+      // Skip explicitly excluded paths
+      if (excludedPaths.includes(edge.node.path)) return null
+
+      return {
+        title: edge.node.context.seoTitle,
+        url: url.resolve(
+          data.site.siteMetadata.siteUrl,
+          path.join(data.site.siteMetadata.pathPrefix, edge.node.path)
+        ),
+      }
+    })
+    .filter(edge => edge !== null)
+
   await fsPromises.writeFile(outputFile, JSON.stringify(pages))
 }
