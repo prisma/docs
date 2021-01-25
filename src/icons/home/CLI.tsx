b05890b07e62b5209b68d3dfa17c2eddd6eb820c
Nilufar Bava <nilubava@gmail.com>
2021-01-25 18:55:29 +0530

    feat: New homepage for docs (#1114)
    Co-authored-by: Matej Hrescak <matej@hrescak.com>
    Co-authored-by: mhwelander <welander@prisma.io>
    Co-authored-by: Martina Welander <mhwelander@users.noreply.github.com>

---
+++ src/icons/home/CLI.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+export default () => (
+  <svg width="65" height="67" viewBox="0 0 65 67" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="1" width="64" height="50" rx="4" fill="#2D3748" />
+    <path
+      d="M23.3887 21.7122L17.6106 21.7122"
+      stroke="#9AE6B4"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M11.625 10.625L16.875 15.875L11.375 21.3749"
+      stroke="#9AE6B4"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
