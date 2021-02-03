0ce6634f9b0931f2efe810db6ee93ced1d661480
Nilufar Bava <nilubava@gmail.com>
2021-02-03 18:30:46 +0530

    fix: Support 3rd level navigation  (#1252)
    * added parent label
    
    * Collapse parent sec

--- src/components/sidebar/treeNode.tsx
+++ src/components/sidebar/treeNode.tsx
@@ -31,13 +31,6 @@ const ListItem = styled.li`
       color: ${p => p.theme.colors.gray900} !important;
     }
 
-    // @media (min-width: 0px) and (max-width: 1024px) {
-    //   color: ${p => p.theme.colors.gray300} !important;
-    //   &:hover {
-    //     color: ${p => p.theme.colors.white} !important;
-    //   }
-    // }
-
     .tag {
       position: absolute;
       right: 0;
@@ -51,10 +44,6 @@ const ListItem = styled.li`
       &.small {
         font-size: 13px;
       }
-      // @media (min-width: 0px) and (max-width: 1024px) {
-      //   background: ${p => p.theme.colors.gray800};
-      //   color: ${p => p.theme.colors.gray500};
-      // }
     }
 
     .item-collapser {
@@ -100,24 +89,14 @@ const ListItem = styled.li`
   .active-item {
     color: ${p => p.theme.colors.blue600} !important;
     font-weight: 700;
-    // @media (min-width: 0px) and (max-width: 1024px) {
-    //   color: ${p => p.theme.colors.gray300} !important;
-    // }
   }
   &.top-level {
     margin-top: ${p => p.theme.space[32]};
-
-    // &:nth-child(1) {
-    //   margin-top: 0;
-    // }
     > a {
       font-size: 1.125rem;
       color: ${p => p.theme.colors.gray900} !important;
       font-weight: 600;
       letter-spacing: -0.01em;
-      // @media (min-width: 0px) and (max-width: 1024px) {
-      //   color: ${p => p.theme.colors.gray100} !important;
-      // }
     }
     > ul {
       margin-top: ${p => p.theme.space[12]};
@@ -168,6 +147,7 @@ const TreeNode = ({
   lastLevel,
   hidePage,
   codeStyle,
+  parentLabel,
 }: any) => {
   const isCollapsed = collapsed[label]
   const collapse = () => {
@@ -176,7 +156,7 @@ const TreeNode = ({
         collapsed[lbl] = collapsed[lbl] == false ? (collapsed[lbl] = true) : collapsed[lbl]
       }
     })
-    setCollapsed(label, false)
+    setCollapsed(label, false, parentLabel)
   }
   const location = useLocation()
 
