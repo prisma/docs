dd125187629d418aa4740e2ce4fa981cceec0426
Nilu <nilubava@gmail.com>
2021-02-11 17:55:05 +0530

    set apiReference as searchable attr

--- src/utils/algolia.js
+++ src/utils/algolia.js
@@ -99,7 +99,7 @@ const unnestFrontmatter = node => {
 }
 
 const settings = {
-  searchableAttributes: ['title', 'heading', 'content'],
+  searchableAttributes: ['title', 'heading', 'content', 'apiReference'],
   attributesToHighlight: ['title', 'heading', 'content'],
   attributesToSnippet: ['title:20', 'heading:20', 'content:25'],
   hitsPerPage: 20,
