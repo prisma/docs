530d56d06fde49777a1560afef11d9431c679bfa
mhwelander <welander@prisma.io>
2021-02-11 13:32:50 +0100

    Native types Caching fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,14 @@ const gatsbyRemarkPlugins = [
       enableCustomId: true,
     },
   },
-
+  {
+    resolve: "gatsby-plugin-netlify",
+    options: {
+      headers: {
+        "/*.(woff|woff2)": ["Cache-Control: public, max-age=31536000"],
+      },
+    },
+  },
   {
     resolve: `gatsby-remark-images`,
     options: {
