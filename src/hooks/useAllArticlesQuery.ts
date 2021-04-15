246684541db8905e7ce4f48d0e0a343724bf04e4
Nilu <nilubava@gmail.com>
2021-04-15 11:29:31 +0200

    fix errors

--- src/hooks/useAllArticlesQuery.ts
+++ src/hooks/useAllArticlesQuery.ts
@@ -17,7 +17,6 @@ export const useAllArticlesQuery = () => {
               dbSwitcher
               hidePage
               codeStyle
-              navTitle
             }
             fields {
               slug
