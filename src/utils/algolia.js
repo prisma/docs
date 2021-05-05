8bf1c7493855bbe07569231de4805035c8401d38
Nilu <nilubava@gmail.com>
2021-05-05 11:55:53 +0200

    algolia indexing

--- src/utils/algolia.js
+++ src/utils/algolia.js
@@ -118,7 +118,7 @@ const queries = [
   {
     query: `
         {
-          allMdx(sort: {fields: fields___slug}) {
+          allMdx {
             edges {
               node {
                 rawBody
