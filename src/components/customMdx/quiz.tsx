fdf7b89c5b6759c4c757ac8153da54386029d3bf
Rich Haines <hello@richardhaines.dev>
2021-05-14 10:41:18 +0200

    styling, colours, disable logic - still not 100% there

--- src/components/customMdx/quiz.tsx
+++ src/components/customMdx/quiz.tsx
@@ -11,16 +11,20 @@ interface QuestionAndAnswer {
   answerOptions: AnswerOptions[]
 }
 
+interface ChosenAnswerProps {
+  isCorrect: boolean;
+}
+
 const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: 'flex-start;
   padding: 1rem;
-  background-color: #edf2f7;
+  background-color: ${p => p.theme.colors.gray200};
   border-top-right-radius: 8px;
   border-bottom-right-radius: 8px;
   min-height: 100px;
-  border-left: 2px solid #2d3748;
+  border-left: 2px solid ${p => p.theme.colors.gray700};
   margin: 2rem 0;
 `
 
@@ -38,7 +42,6 @@ const QuestionTitle = styled.h3`
 `
 
 const AnswersWrapper = styled.label`
-
   & > span {
     align-self: center;
     margin-left: 0.5rem;
@@ -46,14 +49,14 @@ const AnswersWrapper = styled.label`
 `
 
 const ChosenAnswer = styled.div<ChosenAnswerProps>`
-  background-color: ${props => props.isCorrect ? '#A3F5D3' : '#F5B7B7'};
-  padding: 1rem;
+  background-color: ${props => (props.isCorrect ? '#A3F5D3' : '#F5B7B7')};
+  padding: 0.5rem 1rem;
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 1rem auto;
   border-radius: 8px;
-  border: ${props => props.isCorrect ? '2px solid #15bd76' : '2px solid #ff4f56'};
+  border: ${props => (props.isCorrect ? '2px solid #15bd76' : '2px solid #ff4f56')};
 
   & > div {
     display: flex;
@@ -70,7 +73,7 @@ const ButtonsWrapper = styled.div`
   flex-direction: row;
   justify-content: space-between;
   margin: 2rem 0.5rem 0 0.5rem;
-`;
+`
 
 const Row = styled.div`
   display: flex;
@@ -82,7 +85,7 @@ const Row = styled.div`
   & > span {
     font-size: 0.85rem;
   }
-`;
+`
 
 const RadioButtonLabel = styled.label`
   position: absolute;
@@ -91,29 +94,29 @@ const RadioButtonLabel = styled.label`
   height: 20px;
   border-radius: 50%;
   background: white;
-  border: 2px solid #a0aec0;
-`;
-const RadioButton = styled.input`
+  border: 2px solid ${p => p.theme.colors.gray500};
+`
+const RadioButton = styled.input<ChosenAnswerProps>`
   opacity: 0;
   z-index: 1;
   border-radius: 50%;
   width: 20px;
   height: 20px;
-  margin-right: 0.25rem;;
+  margin-right: 0.25rem;
   cursor: pointer;
   &:hover ~ ${RadioButtonLabel} {
-    background: #a0aec0;
+    background: ${p => p.theme.colors.gray500};
     &::after {
-      content: "";
+      content: '';
       display: block;
       border-radius: 50%;
       width: 10px;
       height: 10px;
       margin: 3px;
-      background: #eeeeee;
+      background: ${p => p.theme.colors.gray100};
     }
   }
-  ${(props) =>
+  ${props =>
     props.checked &&
     ` 
     &:checked + ${RadioButtonLabel} {
@@ -132,10 +135,10 @@ const RadioButton = styled.input`
     }
   `}
 
-  ${(props) =>
+  ${props =>
     props.disabled &&
     ` &:disabled + ${RadioButtonLabel} {
-      background: #3182ce;
+      background: #a0aec0;
       border: 1px solid #c3dafe;
       &::after {
         content: "";
@@ -149,7 +152,7 @@ const RadioButton = styled.input`
       }
     }
   `}
-`;
+`
 
 function QuestionMark() {
   return (
@@ -168,67 +171,70 @@ function QuestionMark() {
         <path d="M17.6 30c0 1.102-.895 2-2 2s-2-.898-2-2c0-1.109.895-2 2-2s2 .891 2 2zM15.676 25.977a4.964 4.964 0 01-3.535-1.469c-.945-1.105-1.465-2.359-1.465-3.695s.52-2.59 1.465-3.371l6.688-6.688A3.983 3.983 0 0020 7.926c0-1.07-.416-2.074-1.172-2.828a4 4 0 00-5.654 0A3.958 3.958 0 0012 7.926H8c0-2.137.834-4.148 2.348-5.66 3.02-3.023 8.285-3.02 11.309.004A7.932 7.932 0 0124 7.926a7.942 7.942 0 01-2.344 5.656l-6.688 6.523a.999.999 0 101.707.703h4c0 1.336-.52 2.594-1.465 3.699a4.96 4.96 0 01-3.534 1.47z"></path>
       </g>
     </svg>
-  );
+  )
 }
 
 const Button = styled.button`
-font-size: 1rem;
-font-weight: 600;
-padding: 0.25rem 0.5rem;
-display: inline-block;
-border: solid 2px ${p => p.theme.colors.gray700};
-background: inherit;
-text-align: left;
-border-radius: 5px;
-line-height: 1.5;
-cursor: pointer;
-text-decoration: none;
-outline: none;
-color: ${p => p.theme.colors.gray700};
-transition: 0.2s ease-out;
--webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-&:hover {
-  background: ${p => p.theme.colors.gray600};
-  color: #fff;
-}
-&:active {
-  transform: scale(0.98);
-}
-&:disabled {
-  background: ${p => p.theme.colors.gray400};
+  font-size: 1rem;
+  font-weight: 600;
+  padding: 0.25rem 0.5rem;
+  display: inline-block;
+  border: solid 2px ${p => p.theme.colors.gray700};
+  background: inherit;
+  text-align: left;
+  border-radius: 5px;
+  line-height: 1.5;
+  cursor: pointer;
+  text-decoration: none;
+  outline: none;
   color: ${p => p.theme.colors.gray700};
-  cursor: default;
-}
+  transition: 0.2s ease-out;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  &:hover {
+    background: ${p => p.theme.colors.gray600};
+    color: #fff;
+  }
+  &:active {
+    transform: scale(0.98);
+  }
+  &:disabled {
+    background: ${p => p.theme.colors.gray400};
+    color: ${p => p.theme.colors.gray700};
+    cursor: default;
+  }
 `
 
 const Quiz = ({ question, answerOptions }: QuestionAndAnswer) => {
-  const [chosenAnswer, setChosenAnswer] = React.useState<AnswerOptions>({answer: '', isCorrect: false});
-  const [answerSubmitted, setAnswerSubmitted] = React.useState<boolean>(false);
-  const [disabled, setDisabled] = React.useState<boolean>(false);
+  const [chosenAnswer, setChosenAnswer] = React.useState<AnswerOptions>({
+    answer: '',
+    isCorrect: false,
+  })
+  const [answerSubmitted, setAnswerSubmitted] = React.useState<boolean>(false)
+  const [disabled, setDisabled] = React.useState<boolean>(false)
 
   const handleSelectChange = (event: any) => {
     console.log(event.target.value)
-      setChosenAnswer({
-        answer: event.target.value,
-        isCorrect: false
-      })
+    setChosenAnswer({
+      answer: event.target.value,
+      isCorrect: false,
+    })
   }
 
   const onSubmit = () => {
     setAnswerSubmitted(true)
     setDisabled(true)
-    const foundAnswer = answerOptions.find((answer) => answer.answer === chosenAnswer.answer);
+    const foundAnswer = answerOptions.find(answer => answer.answer === chosenAnswer.answer)
     if (foundAnswer && foundAnswer.isCorrect) {
       const correctAnswerText = `Correct! ${foundAnswer.answer}`
       setChosenAnswer({
         answer: correctAnswerText,
-        isCorrect: true
+        isCorrect: true,
       })
     } else {
       const wrongAnswerText = `Oh no! Please read through the section again.`
       setChosenAnswer({
         answer: wrongAnswerText,
-        isCorrect: false
+        isCorrect: false,
       })
     }
   }
@@ -237,36 +243,37 @@ const Quiz = ({ question, answerOptions }: QuestionAndAnswer) => {
     setAnswerSubmitted(false)
     setChosenAnswer({
       answer: '',
-      isCorrect: false
-    });
+      isCorrect: false,
+    })
     setDisabled(false)
   }
-
 
   return (
     <Wrapper>
       <QuestionTitle>
-      <QuestionMark/>
+        <QuestionMark />
         <span>{question}</span>
       </QuestionTitle>
       {answerOptions.map((item, index) => (
         <AnswersWrapper key={index}>
-        <Row>
-        <RadioButton
-          disabled={disabled}
-          type="radio"
-          name="radio"
-          value={item.answer}
-          checked={chosenAnswer.answer === item.answer}
-          onChange={(event) => handleSelectChange(event)}
-        />
-        <RadioButtonLabel />
-        <span>{item.answer}</span>
-        </Row>
+          <Row>
+            <RadioButton
+              disabled={disabled}
+              type="radio"
+              name="radio"
+              value={item.answer}
+              checked={chosenAnswer.answer === item.answer}
+              onChange={event => handleSelectChange(event)}
+            />
+            <RadioButtonLabel />
+            <span>{item.answer}</span>
+          </Row>
         </AnswersWrapper>
       ))}
-       <ButtonsWrapper>
-        <Button disabled={disabled} onClick={onSubmit}>Submit</Button>
+      <ButtonsWrapper>
+        <Button disabled={disabled} onClick={onSubmit}>
+          Submit
+        </Button>
         <Button onClick={onReset}>Reset</Button>
       </ButtonsWrapper>
       {answerSubmitted ? (
