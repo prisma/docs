8702e6706cf4cfadf5041d248aee741aad730cdc
Pierre-Antoine Mills <pierreantoine.urvoy@gmail.com>
2022-01-28 13:09:38 +0100

    debug

--- src/icons/technologies/Relational.tsx
+++ src/icons/technologies/Relational.tsx
@@ -1,13 +1,7 @@
 import * as React from 'react'
 
 export default () => (
-  <svg
-    width="12"
-    height="15"
-    viewBox="0 0 12 15"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M6 5.59609C8.76142 5.59609 11 4.79021 11 3.79609C11 2.80198 8.76142 1.99609 6 1.99609C3.23858 1.99609 1 2.80198 1 3.79609C1 4.79021 3.23858 5.59609 6 5.59609Z"
       stroke="#A0AEC0"
