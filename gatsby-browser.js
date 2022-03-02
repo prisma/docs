fd1d9bb1d9d138dec07999aad7c1825bd37e230b
Nilu <nilubava@gmail.com>
2022-03-02 15:29:27 +0100

    feat: causes link

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 const { init, trackPage } = require('./src/utils/stats')
 const { goToNav } = require('./src/utils/goToNavItem')
+const { redirects } = require('./config')
 
 exports.onClientEntry = () => {
   init()
@@ -8,4 +9,9 @@ exports.onClientEntry = () => {
 exports.onRouteUpdate = ({ location }) => {
   trackPage(location.pathname)
   goToNav(location.pathname) // This may not work anymore due to the new scroll behavior of the sidebar
+  // const isRedirectablePath = redirects.filter((redirect) => redirect.from === location.href)
+
+  // if (isRedirectablePath[0]) {
+  //   window.location.replace(isRedirectablePath[0].to)
+  // }
 }
