5a91725d6bdcf57b012826b5f05a8492b7423769
Nilufar Bava <nilubava@gmail.com>
2022-04-27 00:38:12 +0200

    feat: redirect anchor links using browser apis (#2954)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,4 +9,24 @@ exports.onClientEntry = () => {
 exports.onRouteUpdate = ({ location }) => {
   trackPage(location.pathname)
   goToNav(location.pathname) // This may not work anymore due to the new scroll behavior of the sidebar
+  const isRedirectablePath = redirects.filter((redirect) => {
+    const redirectFromSplit = redirect.from.split('#')
+    if (redirectFromSplit.length > 1) {
+      const isHashPath =
+        location.href.includes(redirectFromSplit[0]) && location.hash === `#${redirectFromSplit[1]}`
+      return isHashPath
+    }
+    return location.href.includes(redirect.from)
+  })
+  if (isRedirectablePath[0]) {
+    const fromPathWithoutHash = isRedirectablePath[0].from.split('#')
+    const toPathWithoutHash = isRedirectablePath[0].to.split('#')
+    if (fromPathWithoutHash[0] === toPathWithoutHash[0]) {
+      window.location.hash = toPathWithoutHash[1]
+    } else {
+      const newpath = location.href.replace(isRedirectablePath[0].from, isRedirectablePath[0].to)
+      console.log(newpath)
+      window.location.replace(newpath)
+    }
+  }
 }
