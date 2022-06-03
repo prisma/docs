44dd4dc874f8f42f62b01c4365d425ad5455ba6c
Nilufar Bava <nilubava@gmail.com>
2022-06-03 13:25:32 +0200

    cookie (#3226)

--- src/html.js
+++ src/html.js
@@ -6,6 +6,17 @@ export default function HTML(props) {
     <html {...props.htmlAttributes}>
       <head>
         <meta charSet="utf-8" />
+        {/* OneTrust Cookies Consent Notice start for prisma.io  */}
+
+        <script
+          src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
+          data-document-language="true"
+          type="text/javascript"
+          charSet="UTF-8"
+          data-domain-script="22c2e2c0-3df0-4958-8672-1194370ee230"
+        ></script>
+        <script type="text/javascript">function OptanonWrapper() {}</script>
+        {/* OneTrust Cookies Consent Notice end for prisma.io */}
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         {props.headComponents}
       </head>
