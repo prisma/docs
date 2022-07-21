44f54ed5d95232a14afb088f7863337788d74e8d
Nilu <nilubava@gmail.com>
2022-07-21 17:18:50 +0200

    remove fingerprints

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,7 +134,6 @@ let plugins = [
   'gatsby-plugin-remove-trailing-slashes',
   'gatsby-plugin-meta-redirect',
   'gatsby-plugin-page-list',
-  'gatsby-plugin-remove-fingerprints',
   {
     resolve: 'gatsby-plugin-google-tagmanager',
     options: {
