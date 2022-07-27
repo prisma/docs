d673620390c00ad854fc0996593283b9779d646b
Nilu <nilubava@gmail.com>
2022-07-27 19:42:46 +0200

    fix: api function issues

--- api/feedback.js
+++ api/feedback.js
@@ -3,7 +3,7 @@ const { PrismaClient } = require('@prisma/client')
 const client = new PrismaClient()
 
 export default async function handle(req, res) {
-  const body = JSON.parse(req.body)
+  const body = req.body
   if (!body.feedback) {
     throw new Error(`Please provide feedback `)
   }
