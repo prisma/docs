a00cae44d8fc38eddbe911ab5e414199ea8697ec
Nilu <nilubava@gmail.com>
2022-07-28 17:12:56 +0200

    fix: json parse data

--- api/sentiment.js
+++ api/sentiment.js
@@ -3,26 +3,26 @@ const { PrismaClient } = require('@prisma/client')
 const client = new PrismaClient()
 
 export default async function handle(req, res) {
-  const body = req.body
-  if (!body.pageUrl) {
+  const { pageUrl, sentiment } = JSON.parse(req.body)
+  if (!pageUrl) {
     throw new Error(`Please provide a pageUrl`)
   }
 
-  if (!body.sentiment) {
+  if (!sentiment) {
     throw new Error(`Please provide a sentiment`)
   }
 
-  if (!['Happy', 'Unhappy'].includes(body.sentiment)) {
+  if (!['Happy', 'Unhappy'].includes(sentiment)) {
     throw new Error(`Please provide "Happy" or "Unhappy" as the sentiment`)
   }
-  const pageUrl = stripTrailingSlash(body.pageUrl)
+  const pagePath = stripTrailingSlash(pageUrl)
 
   const feedback = await client.feedback.create({
     data: {
-      pageUrl,
+      pageUrl: pagePath,
       ip: req.headers['x-forwarded-for'],
       userAgent: req.headers['user-agent'],
-      sentiment: body.sentiment,
+      sentiment: sentiment,
     },
   })
 
