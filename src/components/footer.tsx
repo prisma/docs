1ab3d297f08a806534c5e64f023f3780596b56f0
carlagn <goncalves@prisma.io>
2022-09-09 10:16:53 +0100

    Lens footer update (#3746)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import NewsLetter from '../components/newsletter'
-import { FooterProps } from '../interfaces/Layout.interface'
 import { WebsiteFooter } from '@prisma/lens/dist/web'
-
-type FooterViewProps = {
-  footerProps: FooterProps
-}
 
 const FooterWrapper = styled.div`
   background: transparent;
@@ -36,11 +30,10 @@ const FooterWrapper = styled.div`
   // }
 `
 
-const FooterSec = ({ footerProps }: FooterViewProps) => {
-  const { newsletter } = footerProps
+const FooterSec = () => {
   return (
     <FooterWrapper>
-      <WebsiteFooter newsletterComponent={<NewsLetter newsletter={newsletter} />} />
+      <WebsiteFooter />
     </FooterWrapper>
   )
 }
