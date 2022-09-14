d6f12561a59b51b9663f6572dcdafc13f2bf3ec0
Nilu <nilubava@gmail.com>
2022-09-14 14:29:56 +0200

    404 page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,6 +11,7 @@ import Footer from './footer'
 import '../styles/layout.css'
 import SidebarLayout from './sidebar'
 import TableOfContents from './toc'
+import Banner from './banner'
 
 interface LayoutContentProps {
   toc: any
@@ -111,6 +112,7 @@ export default function Layout({ children, toc, tocDepth, location, slug, homePa
     <ThemeProvider theme={theme}>
       <LensProvider>
         <MDXProvider components={shortcodes}>
+          <Banner />
           <Header headerProps={header} />
           <Wrapper fullWidth={homePage}>
             <Container fullWidth={homePage}>
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,26 @@
 import * as React from 'react'
-import { Link, HeadFC } from 'gatsby'
+import Layout from '../components/layout'
+import styled from 'styled-components'
 
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+const NotFoundWrapper = styled.div`
+  font-family: 'Inter';
+  margin-top: 200px;
+  padding: ${(p) => p.theme.space[40]};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h1 {
+    font-weight: bold;
+  }
+`
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-}
-
-const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+const NotFoundPage = () => (
+  <Layout>
+    <NotFoundWrapper>
+      <h1>404 | NOT FOUND</h1>
+      You just hit a route that doesn&#39;t exist!
+    </NotFoundWrapper>
+  </Layout>
+)
 
 export default NotFoundPage
-
-export const Head: HeadFC = () => <title>Not found</title>
