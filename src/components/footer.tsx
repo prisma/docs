85bce5a53eeb95520e7e340e3202f7ec965d1d80
Carla Goncalves <goncalves@prisma.io>
2022-09-22 11:02:35 +0100

    Update footer and lens pkg

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -37,10 +37,9 @@ const FooterWrapper = styled.div`
 `
 
 const FooterSec = ({ footerProps }: FooterViewProps) => {
-  const { newsletter } = footerProps
   return (
     <FooterWrapper>
-      <WebsiteFooter newsletterComponent={<NewsLetter newsletter={newsletter} />} />
+      <WebsiteFooter />
     </FooterWrapper>
   )
 }
