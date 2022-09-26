f980618fe1ddb1d1bba9b461c322d35e7240a217
Carla Goncalves <goncalves@prisma.io>
2022-09-26 17:05:05 +0100

    Delete redundant code from footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import NewsLetter from '../components/newsletter'
 import { FooterProps } from '../interfaces/Layout.interface'
 import { WebsiteFooter } from '@prisma/lens/dist/web'
 
@@ -18,22 +17,6 @@ const FooterWrapper = styled.div`
   > div {
     width: 100%;
   }
-  // > * {
-  //   .group {
-  //     a {
-  //       color: currentcolor !important;
-  //       text-decoration: none;
-
-  //       &:hover {
-  //         color: ${(p) => p.theme.colors.gray600} !important;
-  //       }
-  //     }
-  //   }
-  //   @media (min-width: 0px) and (max-width: ${(p) => p.theme.breakpoints.tablet}) {
-  //     flex-direction: column;
-  //     padding: ${(p) => p.theme.fontSizes[48]} 0.5rem;
-  //   }
-  // }
 `
 
 const FooterSec = ({ footerProps }: FooterViewProps) => {
