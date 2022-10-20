1875e022def8438c24f3d2427c80ba18d81aaaf9
Carla Goncalves <goncalves@prisma.io>
2022-10-20 12:05:31 +0100

    Remove seo import from 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import styled from 'styled-components'
-import SEO from '../components/seo'
 
 const NotFoundWrapper = styled.div`
   font-family: 'Inter';
