202c2a59c67240e54d29c3ea0feb6ff80315153a
Nilu <nilubava@gmail.com>
2022-11-03 10:10:49 +0100

    fix: remove trailing slash

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -140,7 +140,7 @@ if (process.env.INDEX_ALGOLIA === 'true') {
 
 const config: GatsbyConfig = {
   pathPrefix: process.env.ADD_PREFIX === 'true' ? docsConfig.gatsby.pathPrefix : '/',
-  //trailingSlash: 'never',
+  trailingSlash: 'never',
   siteMetadata: {
     pathPrefix: docsConfig.gatsby.pathPrefix,
     title: docsConfig.siteMetadata.title,
