09f1afd54e9563d5bf470e5b9cbd203572e382c7
Nilufar Bava <nilubava@gmail.com>
2023-01-26 17:29:04 +0100

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -165,8 +165,8 @@ const config: GatsbyConfig = {
     homepage: docsConfig.homepage,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
+  // If you use VSCode you can also use the GraphQL plugin.
+  // If you want to learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins,
 }
