00c4ea094a482a6e9ec0def2ff052b6a477009e9
Nilu <nilubava@gmail.com>
2023-01-27 19:57:22 +0100

    add baseurl

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   pageShots: {
     // write your pages here, it's an empty array, as we are generating the pages on the fly in gatsby-plugin
-    pages: [],
+    pages: [
+      { path: '/getting-started', name: '-getting-started' },
+      { path: '/concepts', name: '-concepts' },
+    ],
     // you create your pages on the fly here & it is composed with `pages` array
     pagesJsonUrl: 'http://172.17.0.1:9000/lost-pixel.json',
+    baseUrl: 'http://172.17.0.1:9000',
   },
+  //generateOnly: true,
   lostPixelProjectId: 'clb5ek3mm1772001qqg7yban38',
   apiKey: process.env.LOST_PIXEL_API_KEY,
   compareConcurrency: 10,
@@ -14,7 +19,7 @@ module.exports = {
       content: `iframe {
           visibility: hidden;
         }
-    
+
         /* do not show underline animation */
         #toc-holder  a {
           background-size: 0 !important;
@@ -24,7 +29,7 @@ module.exports = {
         section img {
           visibility: hidden;
         }
-    
+
         /* hide cookie banner */
         #onetrust-consent-sdk {
           display: none;
