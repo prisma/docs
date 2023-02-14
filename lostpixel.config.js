76d8aaaf7c08f73595c67ca74882f99d9d0e7f22
Nilufar Bava <nilubava@gmail.com>
2023-02-14 10:35:11 +0100

    Update lostpixel.config.js

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -1,15 +1,8 @@
 module.exports = {
   pageShots: {
-    // write your pages here, it's an empty array, as we are generating the pages on the fly in gatsby-plugin
-    // pages: [
-    //   { path: '/getting-started', name: '-getting-started' },
-    //   { path: '/concepts', name: '-concepts' },
-    // ],
-    // you create your pages on the fly here & it is composed with `pages` array
     pagesJsonUrl: 'http://172.17.0.1:9000/lost-pixel.json',
     baseUrl: 'http://172.17.0.1:9000',
   },
-  //generateOnly: true,
   lostPixelProjectId: 'clb5ek3mm1772001qqg7yban38',
   apiKey: process.env.LOST_PIXEL_API_KEY,
   compareConcurrency: 10,
