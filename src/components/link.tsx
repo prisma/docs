cadcee52db7ac86a80b74ef1fce0da54a95fc896
Carla Goncalves <goncalves@prisma.io>
2023-03-28 11:51:47 +0100

    mend

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -19,19 +19,21 @@ const Link = ({
   !to || isAbsoluteUrl(to) ? (
     <a href={to} {...props} target="__blank" rel="noopener">
       {props.children}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="12"
-        height="12"
-        viewBox="0 0 12 12"
-        fill="currentColor"
-        color="inherit"
-      >
-        <path
-          fill="inherit"
-          d="M6 1h5v5L8.86 3.85 4.7 8 4 7.3l4.15-4.16L6 1Z M2 3h2v1H2v6h6V8h1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1Z"
-        />
-      </svg>
+      {to && isAbsoluteUrl(to) && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="currentColor"
+          color="inherit"
+        >
+          <path
+            fill="inherit"
+            d="M6 1h5v5L8.86 3.85 4.7 8 4 7.3l4.15-4.16L6 1Z M2 3h2v1H2v6h6V8h1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1Z"
+          />
+        </svg>
+      )}
     </a>
   ) : (
     <GatsbyLink
