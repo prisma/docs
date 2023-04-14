91cfba10b24af374bf796fecd4c59df63e97d3b8
Nilufar Bava <nilubava@gmail.com>
2023-04-14 14:41:50 +0200

    Update config.ts (#4666)

--- config.ts
+++ config.ts
@@ -12,6 +12,10 @@ const siteConfig = {
       from: '/foo',
       to: '/bar',
     },
+    {
+      from: '/guides/database/prototyping-schema-db-push',
+      to: '/guides/migrate/prototyping-schema-db-push'
+    }
   ],
   header: {
     secondLevelHeaderMenuItems: [
