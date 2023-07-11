69b915ad7e1acf23ef37f75f45e4048b9c2c355a
Nurul <nurulraza26@gmail.com>
2023-07-11 12:47:05 +0530

    Extracting Mdx file content

--- bot/walk.ts
+++ bot/walk.ts
@@ -1,5 +1,12 @@
 import fs from 'fs'
 import path from 'path'
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkMdx from 'remark-mdx'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxjs from 'remark-mdxjs'
 
 function getMdxFiles(dir: string, excludedFilesPattern: RegExp): string[] {
   const files = fs.readdirSync(dir)
@@ -21,10 +28,29 @@ function getMdxFiles(dir: string, excludedFilesPattern: RegExp): string[] {
 
 // walk directory and find mdx files
 const excludedFilesPattern = /\.(svg|png|jpg|jpeg)$|^404\.mdx$/
-const mdxFiles = getMdxFiles(path.join(__dirname, '..', 'content'), excludedFilesPattern)
-
-for (const file of mdxFiles) {
-  console.log(file)
-}
+let mdxFiles = getMdxFiles(path.join(__dirname, '..', 'content'), excludedFilesPattern)
 
 // TODO: Extract sections from each mdx file
+async function extractSections(mdxFile: string) {
+  const content = fs.readFileSync(mdxFile, 'utf8')
+
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkMdxjs)
+    .use(remarkFrontmatter)
+    .use(remarkMdx)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+
+  try {
+    const file = await processor.process(content)
+    console.log(String(file))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+for (const file of mdxFiles) {
+  console.log(`Extracting sections from ${file}`)
+  extractSections(file)
+}
