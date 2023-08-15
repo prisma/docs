3cd980384992c0ba9476b7d47db1ea115f91333c
Nilufar Bava <nilubava@gmail.com>
2023-08-15 15:26:22 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -98,8 +98,7 @@ const siteConfig = {
         title: 'Prisma Data Platform',
         icon: 'triangle',
         description:
-          'An ecosystem of tools empowering teams to build data-heavy, global-first applications'
-          'An ecosystem of tools to empower teams to easily create and launch data-heavy, global-first software.',
+          'An ecosystem of tools empowering teams to build data-heavy, global-first applications',
         links: [
           {
             url: 'data-platform/accelerate',
