fe12a3eb7ade39745631e8864bf56f61ecd35f6c
Carla Goncalves <goncalves@prisma.io>
2023-08-16 14:24:41 +0100

    Add intersectionobserver active ref to control active element

--- src/components/toc.tsx
+++ src/components/toc.tsx
@@ -106,6 +106,8 @@ const useIntersectionObserver = (
   const depth = tocDepth ?? 2
   const allHeadings = headings
 
+  const intersectionActive = React.useRef<string | undefined>(undefined)
+
   const getKeyByValue: any = (obj: any, value: string) =>
     Object.keys(obj).find((key) => obj[key] === value)
   const deepExists: any = (obj: any, query: string) =>
@@ -153,6 +155,7 @@ const useIntersectionObserver = (
               deepExists(e, visibleId) ? e : false
             )
             setActiveId(secondH[0].url.slice(1).replaceAll('inlinecode', ''))
+            intersectionActive.current = secondH[0].url.slice(1).replaceAll('inlinecode', '')
           } else if (
             visibleHeadings.length &&
             firstH.length &&
@@ -162,11 +165,18 @@ const useIntersectionObserver = (
             parseInt(visibleHeadingN ? visibleHeadingN : '0') > depth + 1
           ) {
             setActiveId(firstH[0].url.slice(1).replaceAll('inlinecode', ''))
+            intersectionActive.current = firstH[0].url.slice(1).replaceAll('inlinecode', '')
           } else {
             setActiveId(firstH[0].url.slice(1).replaceAll('inlinecode', ''))
+            intersectionActive.current = firstH[0].url.slice(1).replaceAll('inlinecode', '')
           }
         } else {
           setActiveId(filteredVisible[0].target.id)
+          intersectionActive.current = filteredVisible[0].target.id
+        }
+      } else {
+        if (intersectionActive.current === Object.keys(headingElementsRef.current)[0]) {
+          setActiveId('')
         }
       }
     }
