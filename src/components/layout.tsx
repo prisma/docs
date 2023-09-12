6c282aa1ebf8ad0618802b1557743b58dfd92ff8
Carla Goncalves <goncalves@prisma.io>
2023-09-12 16:35:45 +0100

    Update styling for headings

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -65,7 +65,7 @@ const Content = styled.article<{ homePage?: boolean; wide?: boolean }>`
     h5,
     h6 {
       &:has(> inlinecode) {
-        line-height: 30px;
+        line-height: 1.3;
       }
     }
   }
