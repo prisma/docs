238b2e714440c10f12b220f1421cb4ada068c169
Nilu <nilubava@gmail.com>
2023-12-13 17:40:45 +0100

    fix: button from a

--- src/components/customLink.tsx
+++ src/components/customLink.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react'
+import styled from 'styled-components'
+
+const CustomButton = styled.button`
+  background: transparent;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+`
 
 const CustomLink = ({ href, ...props }: any) => {
   const path = href.includes('https://www.prisma.io')
     ? href.replace('https://www.prisma.io', '')
     : href
+
+  const goToPath = (e: any) => {
+    //e.preventDefault()
+    window.open(href, '_self')
+  }
   return (
-    <a
-      href={href.includes('https://www.prisma.io') ? 'https://www.prisma.io' + path : path}
+    <CustomButton
+      onClick={goToPath}
+      //href={href}
+      //   href={href.includes('https://www.prisma.io') ? 'https://www.prisma.io' + path : path}
       {...props}
     />
   )
