8ba6addc707d1969745c36716a92e0a700c66912
Nilufar Bava <nilubava@gmail.com>
2023-12-13 18:02:18 +0100

    bugfix: links for mdx with customLink comp (#5542)
    * remove reo script for testing
    
    * header links
    
    * header links
    
    * fix: remove plugins
    
    * link comp
    
    * link comp
    
    * link comp
    
    * link comp
    
    * fix: button from a
    
    * fix: link for mdx

--- src/components/customLink.tsx
+++ src/components/customLink.tsx
@@ -1,30 +1,26 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
-const CustomButton = styled.button`
+const CustomButton = styled.button<{ mdx?: string }>`
   background: transparent;
   border: none;
   padding: 0;
   cursor: pointer;
+
+  ${(props) =>
+    props.mdx &&
+    `
+    color: var(--link-color);
+    cursor: pointer;
+    text-decoration: underline;
+  `};
 `
 
-const CustomLink = ({ href, ...props }: any) => {
-  const path = href.includes('https://www.prisma.io')
-    ? href.replace('https://www.prisma.io', '')
-    : href
-
-  const goToPath = (e: any) => {
-    //e.preventDefault()
+const CustomLink = ({ href, mdx, ...props }: any) => {
+  const goToPath = () => {
     window.open(href, '_self')
   }
-  return (
-    <CustomButton
-      onClick={goToPath}
-      //href={href}
-      //   href={href.includes('https://www.prisma.io') ? 'https://www.prisma.io' + path : path}
-      {...props}
-    />
-  )
+  return <CustomButton onClick={goToPath} {...props} mdx={mdx} />
 }
 
 export default CustomLink
