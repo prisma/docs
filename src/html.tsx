df232d470b3d880d156990eeefa0979951eb1730
Petra Donka <donka@prisma.io>
2024-01-11 14:16:28 +0100

    Unpin Kapa version (#5567)

--- src/html.tsx
+++ src/html.tsx
@@ -9,7 +9,7 @@ const HTML = (props: any) => {
         {/* Kapa.ai script */}
         <script
           defer
-          src="https://widget.kapa.ai/kapa-widget-v1.1.0.bundle.js"
+          src="https://widget.kapa.ai/kapa-widget.bundle.js"
           data-website-id="1b51bb03-43cc-4ef4-95f1-93288a91b560"
           data-project-name="Prisma"
           data-project-color="#2D3748"
