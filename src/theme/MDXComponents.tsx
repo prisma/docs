2b0aa97b7ffd5cd595825844b007b7e226c58dbb
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-01-24 19:03:16 -0600

    tweaks

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
 
+// Import components we'd like to use across Docs
+import Subsections from '@theme/DocCardList'; // DocCardList renamed to Subsections for backwards compat
+
+// Import custom components
 import Admonition from '@site/src/components/shortcodes/admonition'
 import ButtonLink from '@site/src/components/shortcodes/button'
 import TopBlock from '@site/src/components/shortcodes/topBlock'
-import Subsections from '@site/src/components/shortcodes/subSections';
 
 export default {
   // Re-use the default mapping
