02ce6539cfe85dbcb28d3bfe6b629cb087806ce9
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-02-09 15:28:23 -0600

    add missing components

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -9,21 +9,30 @@ import Subsections from '@theme/DocCardList' // DocCardList renamed to Subsectio
 import Admonition from '@site/src/components/shortcodes/admonition'
 import ButtonLink from '@site/src/components/shortcodes/button'
 import TopBlock from '@site/src/components/shortcodes/topBlock'
-import CodeWithResult from '../components/shortcodes/codeWithResult'
-import FileWithIcon from '../components/shortcodes/fileWithIcon'
-import FootNote from '../components/shortcodes/footnote'
-import TabbedContent from '../components/shortcodes/tabbedContent'
-import Tip from '../components/shortcodes/tip'
-import CodeBlock from '../components/shortcodes/codeBlock'
-import ParallelBlocks from '../components/shortcodes/parallelBlocks'
-import SwitchTech from '../components/shortcodes/switchTech'
-import Shortcodes from '../components/shortcodes'
-import Quiz from '../components/shortcodes/quiz'
+import CodeWithResult from '@site/src/components/shortcodes/codeWithResult'
+import FileWithIcon from '@site/src/components/shortcodes/fileWithIcon'
+import FootNote from '@site/src/components/shortcodes/footnote'
+import TabbedContent from '@site/src/components/shortcodes/tabbedContent'
+import Tip from '@site/src/components/shortcodes/tip'
+import CodeBlock from '@site/src/components/shortcodes/codeBlock'
+import ParallelBlocks from '@site/src/components/shortcodes/parallelBlocks'
+import SwitchTech from '@site/src/components/shortcodes/switchTech'
+import Shortcodes from '@site/src/components/shortcodes'
+import Quiz from '@site/src/components/shortcodes/quiz'
+import * as GettingStartedComponents from '@site/src/components/shortcodes/gettingstarted'
+import CockroachDBgradient from '../icons/technologies/CockroachDBgradient'
+import Database from '../icons/Database'
+import MongoDBSimple from '../icons/technologies/MongoDBSimple'
+import MySQLSimple from '../icons/technologies/MySQLSimple'
+import PlanetScale from '../icons/technologies/PlanetScale'
+import PostgresSQLSimple from '../icons/technologies/PostgresSQLSimple'
+import SQLServer from '../icons/technologies/SQLServer'
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
   // add our custom components
+  ...GettingStartedComponents,
   Admonition,
   ButtonLink,
   CodeWithResult,
@@ -39,4 +48,11 @@ export default {
   Quiz,
   NavigationLinksContainer: Shortcodes.NavigationLinksContainer,
   AlgoliaTerm: Shortcodes.AlgoliaTerm,
+  CockroachDBgradient,
+  Database,
+  MongoDBSimple,
+  MySQLSimple,
+  PlanetScale,
+  PostgresSQLSimple,
+  SQLServer,
 }
