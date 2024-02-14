94a88debd459c024751628bbbf760cc29d3d7d8d
meletj <70962362+meletj@users.noreply.github.com>
2024-02-14 13:11:30 +0100

    update landing page text (#5632)
    * update landing page text
    
    * Update config.ts

--- config.ts
+++ config.ts
@@ -91,7 +91,7 @@ const siteConfig = {
     },
     ORMPlatformLinkData: {
       porm: {
-        title: 'Prisma ORM',
+        title: 'Build with Prisma ORM',
         icon: 'database',
         description:
           'Open source Node.js and TypeScript ORM with an intuitive data model, automated migrations, type-safety, and auto-completion.',
@@ -108,10 +108,10 @@ const siteConfig = {
         ],
       },
       pdp: {
-        title: 'Prisma Data Platform',
+        title: 'Grow as your app evolves',
         icon: 'triangle',
         description:
-          'An ecosystem of tools empowering teams to build data-heavy, global-first applications.',
+          'Expand your application capabilities with global database caching, connection pooling and real-time database events.',
         links: [
           {
             url: 'accelerate',
