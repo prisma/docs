777bac9f96bbd5aff77a554b2d821729ea4114dc
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-02-13 20:52:31 -0600

    quick tweak

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://prisma.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs',
+  baseUrl: '/',
 
   trailingSlash: false,
   onBrokenLinks: 'warn',
