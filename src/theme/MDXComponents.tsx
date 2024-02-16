071505df2939ffbe347fc2ebf074c48b66a14262
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-02-16 10:05:21 -0600

    add TODOs and a few more placeholder MDX Components

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -9,38 +9,64 @@ import TabbedContent from '@theme/Tabs'; // Tabs renamed to TabbedContent for ba
 import TabItem from '@theme/TabItem';
 import Link from '@docusaurus/Link';
 
-// do we want to fix this?
+// TODO: do we want to fix this?
 const TopBlock: React.FC<React.PropsWithChildren> = ({ children }) => {
-    return <p>{children}</p>
+  return <p>{children}</p>
 }
 
-// we should fix this
+// TODO: we should fix this
 const CodeWithResult: React.FC<{ children: React.ReactElement[] }> = ({ children }) => {
-    return <>
-        <p>{children[0]}</p>
-        <p>{children[1]}</p>
-    </>
+  return <>
+    <p>{children[0]}</p>
+    <p>{children[1]}</p>
+  </>
 }
 
-// we should fix this
+// TODO: we should fix this
 const SwitchTech: React.FC<React.PropsWithChildren> = ({ children }) => {
-    return <p>{children}</p>
+  return <p>{children}</p>
 }
 
+// TODO: we should fix this
 const ParallelBlocks: React.FC<React.PropsWithChildren> = ({ children }) => {
-    return <>{children}</>
+  return <>{children}</>
+}
+
+type ButtonColor = 'red' | 'green' | 'grey' | 'grey-bg' | 'dark'
+interface ButtonProps {
+  href?: string
+  target?: string
+  block?: boolean
+  color?: ButtonColor
+  disabled?: boolean
+  arrow?: boolean
+  onClick?: any
+  arrowLeft?: boolean
+  theme?: any
+}
+
+// TODO: we should fix this
+const ButtonLink: React.FC<React.PropsWithChildren<ButtonProps>> = ({ children, href }) => {
+  return <Link to={href}>{children}</Link>
+}
+
+// TODO: we should fix this
+const NavigationLinksContainer: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return <>{children}</>
 }
 
 export default {
-    // Re-use the default mapping
-    ...MDXComponents,
-    Subsections,
-    Admonition,
-    TabbedContent,
-    TabItem,
-    Link,
-    TopBlock,
-    CodeWithResult,
-    SwitchTech,
-    ParallelBlocks,
+  // Re-use the default mapping
+  ...MDXComponents,
+  Subsections,
+  Admonition,
+  TabbedContent,
+  TabItem,
+  Link,
+  TopBlock,
+  CodeWithResult,
+  SwitchTech,
+  ParallelBlocks,
+  ButtonLink,
+  NavigationLinksContainer,
 };
