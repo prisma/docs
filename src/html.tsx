bc288f422033d05e2b7b8047ffec82c5b5017a30
Petra Donka <donkapetra@gmail.com>
2024-03-13 22:17:22 +0100

    Try userguiding

--- src/html.tsx
+++ src/html.tsx
@@ -43,15 +43,13 @@ const HTML = (props: any) => {
         <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
         {props.postBodyComponents}
 
-        {/* Start of Reo Javascript */}
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html:
-              '!function(){var e,t,n;e="dea12ffb2578f27",t=function(){Reo.init({ clientID: "dea12ffb2578f27" })},(n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.async=!0,n.onload=t,document.head.appendChild(n)}();',
+              '(function(g,u,i,d,e,s){g[e]=g[e]||[];var f=u.getElementsByTagName(i)[0];var k=u.createElement(i);k.async=true;k.src='https://static.userguiding.com/media/user-guiding-'+s+'-embedded.js';f.parentNode.insertBefore(k,f);if(g[d])return;var ug=g[d]={q:[]};ug.c=function(n){return function(){ug.q.push([n,arguments])};};var m=['previewGuide','finishPreview','track','identify','hideChecklist','launchChecklist'];for(var j=0;j<m.length;j+=1){ug[m[j]]=ug.c(m[j]);}})(window,document,'script','userGuiding','userGuidingLayer','SSG79689XZ2ID');',
           }}
         ></script>
-        {/* End of Reo Javascript */}
       </body>
     </html>
   )
