3a126a14564eb05bdf6201a7892858af811b8c80
Prisma <goncalves@prisma.io>
2024-03-27 11:33:15 +0000

    add FA icons to footer

---
+++ src/theme/Footer/LinkItem/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import isInternalUrl from '@docusaurus/isInternalUrl';
+import { Icon } from '@site/src/components/Icon';
+export default function FooterLinkItem({item}) {
+  const {to, href, label, prependBaseUrlToHref, ...props} = item;
+  const toUrl = useBaseUrl(to);
+  const normalizedHref = useBaseUrl(href, {forcePrependBaseUrl: true});
+  return (
+    <Link
+      className="footer__link-item"
+      {...(href
+        ? {
+            href: prependBaseUrlToHref ? normalizedHref : href,
+          }
+        : {
+            to: toUrl,
+          })}
+      {...props}>
+      {label}
+      {href && !isInternalUrl(href) && <Icon icon='fa-solid fa-arrow-up-right' size='1rem'/>}
+    </Link>
+  );
+}
