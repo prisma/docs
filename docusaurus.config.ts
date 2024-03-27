846cf0e3622e1d569dbf65c1e9361270f51af743
Prisma <goncalves@prisma.io>
2024-03-27 17:38:59 +0000

    push admonition

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -84,7 +84,7 @@ const config: Config = {
         },
         blog: false,
         theme: {
-          customCss: ['./src/css/custom.css', './src/css/all.css', './src/css/theming.css'],
+          customCss: ['./src/css/custom.css', './src/css/admonition.css', './src/css/all.css', './src/css/theming.css'],
         },
       } satisfies Preset.Options,
     ],
