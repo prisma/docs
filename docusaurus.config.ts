0f16129541769969035ab08e036f42230fda462e
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-03-29 10:42:43 -0500

    make baseUrl /docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://prisma.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs',
 
   trailingSlash: false,
   onBrokenLinks: 'warn',
@@ -80,10 +80,9 @@ const config: Config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/prisma/docs',
         },
         blog: false,
         theme: {
