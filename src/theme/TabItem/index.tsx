d084cd88eba9e54e54c8d0be0c34dba2e5067c85
Prisma <goncalves@prisma.io>
2024-04-01 16:22:15 +0100

    update table widths

--- src/theme/TabItem/index.tsx
+++ src/theme/TabItem/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
-export default function TabItem({children, hidden, className}) {
+export default function TabItem({children, hidden, className, code = true }) {
   return (
     <div
       role="tabpanel"
       className={clsx(styles.tabItem, className)}
       {...{hidden}}>
-      <div className={styles.children}>{children}</div>
+      <div className={code ? styles.codeChildren : styles.children}>{children}</div>
     </div>
   );
 }
