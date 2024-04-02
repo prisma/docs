3e2f10f0a65d87fbd7a998774d0518c286da00d9
Prisma <goncalves@prisma.io>
2024-04-02 13:00:09 +0100

    update mobile sidebar

---
+++ src/theme/Navbar/MobileSidebar/Header/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {useNavbarMobileSidebar} from '@docusaurus/theme-common/internal';
+import {translate} from '@docusaurus/Translate';
+import NavbarColorModeToggle from '@theme/Navbar/ColorModeToggle';
+import IconClose from '@theme/Icon/Close';
+import NavbarLogo from '@theme/Navbar/Logo';
+import Link from '@docusaurus/Link';
+import styles from "./styles.module.scss";
+
+function CloseButton() {
+  const mobileSidebar = useNavbarMobileSidebar();
+  return (
+    <button
+      type="button"
+      aria-label={translate({
+        id: 'theme.docs.sidebar.closeSidebarButtonAriaLabel',
+        message: 'Close navigation bar',
+        description: 'The ARIA label for close button of mobile sidebar',
+      })}
+      className="clean-btn navbar-sidebar__close"
+      onClick={() => mobileSidebar.toggle()}>
+      <IconClose color="var(--ifm-color-emphasis-600)" />
+    </button>
+  );
+}
+export default function NavbarMobileSidebarHeader() {
+  return (
+    <div className="navbar-sidebar__brand">
+      <NavbarLogo />
+      <Link to="/" className={styles.docsLink}>/docs</Link>
+      <NavbarColorModeToggle className="margin-right--md" />
+      <CloseButton />
+    </div>
+  );
+}
