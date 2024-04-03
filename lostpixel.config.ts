fa48e7b5ccbd0f3e8030c5c391bf8be4beb836c0
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-04-03 15:38:32 -0500

    don't test multiple viewports for now

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -5,7 +5,6 @@ export const config: CustomProjectConfig = {
     pages: [],
     pagesJsonUrl: './lost-pixel-pages.json',
     baseUrl: 'http://172.17.0.1:3000',
-    breakpoints: [640, 1024],
   },
   lostPixelProjectId: 'clb5ek3mm1772001qqg7yban38',
   apiKey: process.env.LOST_PIXEL_API_KEY,
