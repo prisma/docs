9029c357c697e2ee5085eb130e7ff050d7683de6
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-04-02 20:08:36 -0500

    try Idea 2 again

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://prisma.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   trailingSlash: false,
   onBrokenLinks: 'warn',
@@ -79,7 +79,7 @@ const config: Config = {
           filename: 'sitemap.xml',
         },
         docs: {
-          // routeBasePath: '/',
+          routeBasePath: '/',
           sidebarPath: './sidebars.ts',
           // Remove this to remove the "edit this page" links.
           editUrl:
