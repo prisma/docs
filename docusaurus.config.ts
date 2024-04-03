d8c6a4f8f79d396043b7cbe2b0cf9c0d7685efc8
Prisma <goncalves@prisma.io>
2024-04-03 17:01:51 +0100

    attempt build fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,8 +14,8 @@ const config: Config = {
   baseUrl: '/',
 
   trailingSlash: false,
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
