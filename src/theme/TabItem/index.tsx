8717ba4cc0a33fa2f409eb9dfbd8b63f914305f4
Prisma <goncalves@prisma.io>
2024-04-04 11:01:53 +0100

    DA-149: fix tables inside tab containers

--- src/theme/TabItem/index.tsx
+++ src/theme/TabItem/index.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
-export default function TabItem({children, hidden, className, code = true }) {
+export default function TabItem({children, hidden, className, code = true, table = false }) {
   return (
     <div
       role="tabpanel"
       className={clsx(styles.tabItem, className)}
       {...{hidden}}>
-      <div className={code ? styles.codeChildren : styles.children}>{children}</div>
+      <div className={
+        clsx(code ? styles.codeChildren : styles.children, table && styles.tableChildren)
+      }>{children}</div>
     </div>
   );
 }
