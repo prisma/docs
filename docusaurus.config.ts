df015b52b86fea247e577bcae3bf2bdfe4afa48c
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-04-05 22:23:36 -0500

    update wrangler and add wrangler.toml

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://www.prisma.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: process.env.DOCUSAURUS_BASE_URL ?? '/',
 
   trailingSlash: false,
   onBrokenLinks: 'warn',
