0c98d332eea00df07d666c0cb6dbf8723e6c2293
Prisma <goncalves@prisma.io>
2024-04-08 14:49:32 +0100

    DA-239: add  prop to blockcontent

--- src/theme/CodeBlock/Content/Element.tsx
+++ src/theme/CodeBlock/Content/Element.tsx
@@ -5,13 +5,13 @@ import Container from '../Container';
 // <pre> tags in markdown map to CodeBlocks. They may contain JSX children. When
 // the children is not a simple string, we just return a styled block without
 // actually highlighting.
-export default function CodeBlockJSX({children, className}) {
+export default function CodeBlockJSX({children, className, wrap = false}) {
   return (
     <Container
       as="pre"
       tabIndex={0}
       className={clsx(styles.codeBlockStandalone, 'thin-scrollbar', className)}>
-      <code className={styles.codeBlockLines}>{children}</code>
+      <code className={clsx(styles.codeBlockLines, wrap && styles.wrap)}>{children}</code>
     </Container>
   );
 }
