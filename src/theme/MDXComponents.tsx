bebdbea78fbe4ef50f62ce7695d7a77d08a55005
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-04-09 16:22:06 -0600

    Docusaurus: remove title from TopBlock (#5844)
    removes title from TopBlock and instead uses the default Docusaurus title but styled

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -14,12 +14,11 @@ import { useLocation } from '@docusaurus/router';
 import styles from "./styles.module.scss"
 
 // TODO: do we want to fix this?
-const TopBlock: React.FC<React.PropsWithChildren> = ({ children, langSwitcher, dbSwitcher, slug, title }: any) => {
+const TopBlock: React.FC<React.PropsWithChildren> = ({ children, langSwitcher, dbSwitcher, slug }: any) => {
   const location = useLocation()
   return (
     <>
       <section className="top-section">
-        {title && <header><h1>{title}</h1></header>}
         <TopSection
           location={location}
           langSwitcher={langSwitcher}
@@ -34,7 +33,7 @@ const TopBlock: React.FC<React.PropsWithChildren> = ({ children, langSwitcher, d
 
 // TODO: we should fix this
 const CodeWithResult: React.FC<{ children: React.ReactElement[] }> = ({ children, outputResultText, ...rest }: any) => {
-const [show, setShow] = useState<boolean>(false)
+  const [show, setShow] = useState<boolean>(false)
   return <div className={styles.codeWithResult} {...rest}>
     <div className={styles.cmd}>{children[0]}</div>
     <div className={styles.result}>
@@ -101,8 +100,8 @@ const StyledLink: React.FC<React.PropsWithChildren> = ({ children, ...props }) =
   else
     return <a {...props} target='_blank' rel='openeer noreferrer'>
       {children}
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 12 12" style={{paddingLeft: `3px`}}>
-        <path fill="currentColor" d="M6 1h5v5L8.86 3.85 4.7 8 4 7.3l4.15-4.16zM2 3h2v1H2v6h6V8h1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 12 12" style={{ paddingLeft: `3px` }}>
+        <path fill="currentColor" d="M6 1h5v5L8.86 3.85 4.7 8 4 7.3l4.15-4.16zM2 3h2v1H2v6h6V8h1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1" />
       </svg>
     </a>
 }
