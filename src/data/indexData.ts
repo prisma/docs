a71c03040607c4b808be915d92c087ff61cfa13c
Petra Donka <donkapetra@gmail.com>
2024-04-12 03:11:14 +0200

    Fix PlanetScale capitalization (#5867)

--- src/data/indexData.ts
+++ src/data/indexData.ts
@@ -156,7 +156,7 @@ export const DatabaseData = [
     url: '/orm/overview/databases/cockroachdb',
   },
   {
-    title: 'Planetscale',
+    title: 'PlanetScale',
     icon: "/img/technologies/planetscale.svg",
     darkIcon: "planetscaledark",
     url: '/orm/overview/databases/planetscale',
