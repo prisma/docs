a58b86bd2bcf182ee95c62dc0b145915c249d387
Marc Hess <marc@marchess.dev>
2024-05-10 15:05:52 -0400

    fixed build errors due to location not being set

--- src/theme/DocSidebar/Desktop/index.tsx
+++ src/theme/DocSidebar/Desktop/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import clsx from 'clsx';
-import { useThemeConfig } from '@docusaurus/theme-common';
-import Logo from '@theme/Logo';
-import CollapseButton from '@theme/DocSidebar/Desktop/CollapseButton';
-import Content from '@theme/DocSidebar/Desktop/Content';
-import type { Props } from '@theme/DocSidebar/Desktop';
-import { Promo } from '@site/src/components/promo';
+import React from "react";
+import clsx from "clsx";
+import { useThemeConfig } from "@docusaurus/theme-common";
+import Logo from "@theme/Logo";
+import CollapseButton from "@theme/DocSidebar/Desktop/CollapseButton";
+import Content from "@theme/DocSidebar/Desktop/Content";
+import type { Props } from "@theme/DocSidebar/Desktop";
+import { Promo } from "@site/src/components/promo";
+import { useLocation } from "@docusaurus/router";
 
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 function DocSidebarDesktop({ path, sidebar, onCollapse, isHidden }: Props) {
   const {
@@ -16,6 +17,8 @@ function DocSidebarDesktop({ path, sidebar, onCollapse, isHidden }: Props) {
       sidebar: { hideable },
     },
   } = useThemeConfig();
+
+  let location = useLocation();
 
   return (
     <div
@@ -26,7 +29,7 @@ function DocSidebarDesktop({ path, sidebar, onCollapse, isHidden }: Props) {
       )}
     >
       {hideOnScroll && <Logo tabIndex={-1} className={styles.sidebarLogo} />}
-      {location.pathname.split('/')[1] == 'orm' && <Promo />}
+      {location.pathname.split("/")[1] == "orm" && <Promo />}
       <Content path={path} sidebar={sidebar} />
       {hideable && <CollapseButton onClick={onCollapse} />}
     </div>
