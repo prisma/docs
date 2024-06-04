fcd25fcec65977c631450085e43fef609d768163
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-06-04 14:29:24 -0500

    fix(sidebar): introduce a fixed height for sidebar promo (#6064)
    * introduce a fixed height for sidebar promo
    
    * hide overflow

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -7,6 +7,15 @@ export const config: CustomProjectConfig = {
     baseUrl: "http://172.17.0.1:3000",
     mask: [{ selector: ".sidebar-promo" }, { selector: "article img" }],
   },
+  beforeScreenshot: async (page) => {
+    await page.addStyleTag({
+      content: `.sidebar-promo {
+          height: 50px;
+          overflow: hidden;
+        }
+        `,
+    })
+  },
   lostPixelProjectId: "clb5ek3mm1772001qqg7yban38",
   apiKey: process.env.LOST_PIXEL_API_KEY,
   compareConcurrency: 10,
