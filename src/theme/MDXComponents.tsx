12d01a0a7e84369b36ebffa302fa0f36a330f5f5
Carla Goncalves <goncalves@prisma.io>
2024-06-26 17:03:12 +0100

    Cleanup mdxcomponent for external link

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -13,6 +13,7 @@ import CollapseBox from "@site/src/components/collapsible";
 import TopSection from "@site/src/components/topSection";
 import { useLocation } from "@docusaurus/router";
 import styles from "./styles.module.scss";
+import clsx from "clsx";
 
 // TODO: do we want to fix this?
 const TopBlock: React.FC<React.PropsWithChildren> = ({
@@ -119,20 +120,8 @@ const StyledLink: React.FC<React.PropsWithChildren<ComponentProps<"a">>> = ({
     return <Link {...props}>{children}</Link>;
   else
     return (
-      <a {...props} target="_blank" rel="openeer noreferrer" className={styles.externalLink}>
+      <a {...props} target="_blank" rel="openeer noreferrer" className={clsx(props.className, styles.externalLink)}>
         {children}
-        {/* <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 12 12"
-          style={{ paddingLeft: `3px` }}
-        >
-          <path
-            fill="currentColor"
-            d="M6 1h5v5L8.86 3.85 4.7 8 4 7.3l4.15-4.16zM2 3h2v1H2v6h6V8h1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1"
-          />
-        </svg> */}
       </a>
     );
 };
