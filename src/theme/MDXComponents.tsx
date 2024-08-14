d58a429859f3f0fd4c16ce6dee30407ce556c3a7
Carla Goncalves <goncalves@prisma.io>
2024-08-14 13:25:42 +0100

    Update link color

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -139,6 +139,19 @@ const NavigationLinksContainer: React.FC<React.PropsWithChildren> = ({ children 
   return <>{children}</>;
 };
 
+const ExternalIcon = () => <svg
+xmlns="http://www.w3.org/2000/svg"
+width="16"
+height="16"
+viewBox="0 0 12 12"
+>
+<path
+  color="inherit"
+  fill="currentColor"
+  d="M6 1h5v5L8.86 3.85 4.7 8 4 7.3l4.15-4.16zM2 3h2v1H2v6h6V8h1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1"
+/>
+</svg>
+
 const StyledLink: React.FC<React.PropsWithChildren<ComponentProps<"a">>> = ({
   children,
   ...props
@@ -150,6 +163,7 @@ const StyledLink: React.FC<React.PropsWithChildren<ComponentProps<"a">>> = ({
     return (
       <a {...props} target="_blank" rel="openeer noreferrer" className={clsx(props.className, styles.externalLink)}>
         {children}
+        <ExternalIcon />  
       </a>
     );
 };
