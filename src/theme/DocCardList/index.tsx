f75c92e59868f57761b8d49829c80ef9546d183b
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2024-12-20 17:05:40 -0600

    fix category slicing

--- src/theme/DocCardList/index.tsx
+++ src/theme/DocCardList/index.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import clsx from 'clsx';
+import React, { useEffect, useState } from "react";
+import clsx from "clsx";
 import {
   useCurrentSidebarCategory,
   filterDocCardListItems,
-} from '@docusaurus/plugin-content-docs/client';
-import DocCard from '@theme/DocCard';
-import type {Props} from '@theme/DocCardList';
+} from "@docusaurus/plugin-content-docs/client";
+import DocCard from "@theme/DocCard";
+import type { Props } from "@theme/DocCardList";
 
-function DocCardListForCurrentSidebarCategory({className}: Props) {
+function DocCardListForCurrentSidebarCategory({ className }: Props) {
   const category = useCurrentSidebarCategory();
   return <DocCardList items={category.items} className={className} />;
 }
 
 export default function DocCardList(props: Props): JSX.Element {
-  const {items, className} = props;
+  const { items, className } = props;
   if (!items) {
     return <DocCardListForCurrentSidebarCategory {...props} />;
   }
-  const filteredItems = filterDocCardListItems(items);
+  const [filteredItems, setFilteredItems] = useState<any>(filterDocCardListItems(items));
+  useEffect(() => {
+    setFilteredItems(filteredItems.filter((e: any) => e?.href?.slice(0, -1) !== location.pathname));
+  }, [items]);
   return (
-    <section className={clsx('row', className)}>
+    <section className={clsx("row", className)}>
       {filteredItems.map((item, index) => (
         <article key={index} className="col col--6 margin-bottom--lg">
           <DocCard item={item} />
