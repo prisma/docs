ae897668fabccc00cc3ca2ea164962c1095830e1
Aidan McAlister <105178005+aidankmcalister@users.noreply.github.com>
2025-04-08 21:48:56 +0200

    Fix: Docs Issues (#6815)
    * (Reference) removed
    
    * DA-3083 Highlight colors updated
    
    * DA-3088 Image gap fixed
    
    * DA-3080 @prisma/client -> .../generated/prisma
    
    * DA-3078 CLI global install warning
    
    * Optimised images with calibre/image-actions
    
    * DA-3079 Schema folder link
    
    * DA-3086 Hash fragments re-applied
    
    * Retriggering checks
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -1,21 +1,17 @@
-import React, {useEffect, type ReactNode} from 'react';
-import clsx from 'clsx';
-import ErrorBoundary from '@docusaurus/ErrorBoundary';
-import {
-  PageMetadata,
-  SkipToContentFallbackId,
-  ThemeClassNames,
-} from '@docusaurus/theme-common';
-import {useKeyboardNavigation} from '@docusaurus/theme-common/internal';
-import SkipToContent from '@theme/SkipToContent';
-import AnnouncementBar from '@theme/AnnouncementBar';
-import Navbar from '@theme/Navbar';
-import Footer from '@theme/Footer';
-import LayoutProvider from '@theme/Layout/Provider';
-import ErrorPageContent from '@theme/ErrorPageContent';
-import type {Props} from '@theme/Layout';
-import styles from './styles.module.css';
-import { useHistory, useLocation } from '@docusaurus/router';
+import React, { useEffect, type ReactNode } from "react";
+import clsx from "clsx";
+import ErrorBoundary from "@docusaurus/ErrorBoundary";
+import { PageMetadata, SkipToContentFallbackId, ThemeClassNames } from "@docusaurus/theme-common";
+import { useKeyboardNavigation } from "@docusaurus/theme-common/internal";
+import SkipToContent from "@theme/SkipToContent";
+import AnnouncementBar from "@theme/AnnouncementBar";
+import Navbar from "@theme/Navbar";
+import Footer from "@theme/Footer";
+import LayoutProvider from "@theme/Layout/Provider";
+import ErrorPageContent from "@theme/ErrorPageContent";
+import type { Props } from "@theme/Layout";
+import styles from "./styles.module.css";
+import { useHistory, useLocation } from "@docusaurus/router";
 
 export default function Layout(props: Props): ReactNode {
   const {
@@ -42,13 +38,13 @@ export default function Layout(props: Props): ReactNode {
       const storedParams = localStorage.getItem("utm_params");
 
       if (storedParams) {
-        history.replace(`${location.pathname}?${storedParams}`);
+        history.replace(`${location.pathname}?${storedParams}${location.hash}`);
       }
     } else {
       localStorage.setItem("utm_params", currentParams.toString());
     }
   }, [location, history]);
-  
+
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       const isKapaModalOpen = document.querySelector('#__docusaurus[aria-hidden="true"]');
@@ -74,11 +70,8 @@ export default function Layout(props: Props): ReactNode {
 
       <div
         id={SkipToContentFallbackId}
-        className={clsx(
-          ThemeClassNames.wrapper.main,
-          styles.mainWrapper,
-          wrapperClassName,
-        )}>
+        className={clsx(ThemeClassNames.wrapper.main, styles.mainWrapper, wrapperClassName)}
+      >
         <ErrorBoundary fallback={(params) => <ErrorPageContent {...params} />}>
           {children}
         </ErrorBoundary>
