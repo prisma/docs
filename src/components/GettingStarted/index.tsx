c2671d724f4fb97d3c88f46c3594bce66dc2b010
Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
2025-11-16 01:37:11 +0600

    feat: restructure getting started side nav (#7245)
    * feat(docs): add youtube embeded link to blog post (#7220)
    
    Co-authored-by: Arthur Gamby <arthurgamby@Mac-002.lan>
    
    * feat(docs): add quick  section to blog after the prompt (#7221)
    
    Co-authored-by: Arthur Gamby <arthurgamby@Mac-002.lan>
    
    * DC-5242 Astro Better-Auth Guide (#7215)
    
    * doc created
    
    * nextjs betterauth fixed
    
    * guide broken down into manageable steps
    
    * image added
    
    * Optimised images with calibre/image-actions
    
    * image updated
    
    * Optimised images with calibre/image-actions
    
    * lychee only comments on broken links
    
    * config updated
    
    * lychee updated based on CR comment
    
    * chore: trigger CI checks
    
    * ignore gnu
    
    * fix: update naming to better-auth DC-6120
    
    * Optimised images with calibre/image-actions
    
    * chore: shorten word
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Ankur Datta <64993082+ankur-arch@users.noreply.github.com>
    
    * feat: restructure getting started side nav
    
    * DC-5841 Removed Linkspector (#7231)
    
    * removed linkspector
    
    * retrigger
    
    * retrigger
    
    * retrigger
    
    * Update label for Prisma Postgres tab (#7236)
    
    * fix: content changes for getting started page (#7216)
    
    * fix: content changes for getting started page
    
    * fix: add redirects
    
    * getting started checkpoint
    
    * css styles fixed
    
    * updates
    
    ---------
    
    Co-authored-by: Aidan McAlister <aidankmcalister@gmail.com>
    Co-authored-by: Aidan McAlister <105178005+aidankmcalister@users.noreply.github.com>
    
    * DC-5820 AI Agents Served Markdown (#7237)
    
    * ai crawler check successful
    
    * ai crawlers checked
    
    * update
    
    * added anthropic
    
    * middleware update
    
    * improve detection
    
    ---------
    
    Co-authored-by: Mike Hartington <mikehartington@gmail.com>
    
    * feat: add prisma-orm quickstarts
    
    * fix: update times and add proper links
    
    * fix: instropspect changes
    
    * feat: add get started from prisma orm page
    
    * feat: add other orms
    
    * fix: update other tools + ppg
    
    * fix: add more clarity
    
    * fix: add prisma postgres
    
    * feat: clear migrate from early access
    
    * fix: add to existing dbs sections
    
    * Remove MCP server exploration tip (#7241)
    
    Removed tip about using Cloudflare's AI Playground for MCP server exploration, as it no longer seems to reliably work.
    
    * fix: clean-up docs files
    
    * fix: typeorm missing urls
    
    * fix: broken link
    
    * fix: update titles
    
    * fix: clear redirect loop (#7250)
    
    * fix: add generate step + sqlite fixes
    
    * fix: clean-up redirects file DC-6228
    
    * fix: clean-up unnecessary file
    
    * fix: remove unknown word
    
    * fix: add links
    
    ---------
    
    Co-authored-by: Arthur <arthur_gamby@hotmail.fr>
    Co-authored-by: Arthur Gamby <arthurgamby@Mac-002.lan>
    Co-authored-by: Aidan McAlister <105178005+aidankmcalister@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Petra Donka <donkapetra@gmail.com>
    Co-authored-by: Aidan McAlister <aidankmcalister@gmail.com>
    Co-authored-by: Mike Hartington <mikehartington@gmail.com>

--- src/components/GettingStarted/index.tsx
+++ src/components/GettingStarted/index.tsx
@@ -67,6 +67,36 @@ export const SignalStream = ({ color, height, width }: any) => (
   </svg>
 );
 
+export const Plus = ({ color, height, width }: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 448 512"
+    height={height ?? "16"}
+    width={width ?? "18"}
+    style={{ marginRight: `4px`, transform: `translateY(2px)` }}
+  >
+    <path
+      fill={color ? color : "currentColor"}
+      d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM200 344l0-64-64 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l64 0 0-64c0-13.3 10.7-24 24-24s24 10.7 24 24l0 64 64 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-64 0 0 64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"
+    />
+  </svg>
+);
+
+export const Plug = ({ color, height, width }: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 384 512"
+    height={height ?? "16"}
+    width={width ?? "18"}
+    style={{ marginRight: `4px`, transform: `translateY(2px)` }}
+  >
+    <path
+      fill={color ? color : "currentColor"}
+      d="M96 0C113.7 0 128 14.3 128 32l0 96-64 0 0-96c0-17.7 14.3-32 32-32zM288 0c17.7 0 32 14.3 32 32l0 96-64 0 0-96c0-17.7 14.3-32 32-32zM32 160l320 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l0 32c0 77.4-55 142-128 156.8l0 67.2c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-67.2C87 398 32 333.4 32 256l0-32c-17.7 0-32-14.3-32-32s14.3-32 32-32z"
+    />
+  </svg>
+);
+
 export const PrismaPostgres = ({ color, height, width }: any) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -119,6 +149,23 @@ export const LinkCard = ({ icon, title, desc, link, highlight }: any) => {
         <h6>{title}</h6>
       </div>
       <p>{desc}</p>
+    </Link>
+  );
+};
+
+export const QuickstartLinkCard = ({ icon, title, desc, link, highlight, children }: any) => {
+  const linkCardRef = useRef(null);
+  return (
+    <Link
+      to={link}
+      ref={linkCardRef}
+      className={clsx(styles.linkCardWrapper, highlight && styles.linkCardHighlight)}
+    >
+      <div className={styles.title}>
+        {icon && <Icon icon={icon} btn="left" size="18px" />}
+        <h2>{title}</h2>
+      </div>
+      {children ?? <p>{desc}</p>}
     </Link>
   );
 };
